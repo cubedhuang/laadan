ba2a5e67be297cb506fa2072fcf0ea6ff77ce3e1
cubedhuang <99danielh@gmail.com>
2023-12-29 21:25:58 -0500

    add x

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -189,12 +189,31 @@
 {/if}
 
 <Popup bind:value={selectedWord} let:value={word}>
-	<h2 class="text-2xl font-bold">
-		{word.word}
-		<span class="font-normal text-base text-gray-400">
-			{partOfSpeechNames[word.partOfSpeech]}
-		</span>
-	</h2>
+	<div class="flex gap-2 items-start justify-between">
+		<h2 class="text-2xl font-bold">
+			{word.word}
+			<span class="font-normal text-base text-gray-400">
+				{partOfSpeechNames[word.partOfSpeech]}
+			</span>
+		</h2>
+
+		<button
+			class="interactive p-1 shrink-0"
+			aria-label="close"
+			on:click={() => (selectedWord = null)}
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 20 20"
+				fill="currentColor"
+				class="w-5 h-5"
+			>
+				<path
+					d="M6.28 5.22a.75.75 0 0 0-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 1 0 1.06 1.06L10 11.06l3.72 3.72a.75.75 0 1 0 1.06-1.06L11.06 10l3.72-3.72a.75.75 0 0 0-1.06-1.06L10 8.94 6.28 5.22Z"
+				/>
+			</svg>
+		</button>
+	</div>
 
 	{#if word.successor}
 		<p class="mt-2">
